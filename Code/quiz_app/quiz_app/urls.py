34a57d7174c31f3ae837edd30ef51c36bf4f327b
Aditya2830 <137597258+Aditya2830@users.noreply.github.com>
2025-04-22 12:00:10 +0530

    Delete Code/quiz_app directory

--- Code/quiz_app/quiz_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('generate-quiz/', views.generate_quiz, name='generate_quiz'),
-] 
